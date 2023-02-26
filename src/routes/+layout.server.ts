3e86df7395795ec6518d12c3c7bd4e53e524e58d
Conner Chyung <connerchyung@gmail.com>
2023-02-25 22:17:27 -0700

    image pull

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -12,15 +12,27 @@ export async function load({ params, request }) {
 	const productQuery = await storefrontClient.query({
 		data: `{
       products (first: 10) {
-        edges {
-          node {
-            id
-            title
-            descriptionHtml
+            edges {
+              node {
+                id
+                title
+                description
+                descriptionHtml      
+                images(first: 10) {
+                  edges {
+                    node {
+                      url
+                      height
+                      width
+                      altText
+                    }
+                  }
+                }
+              }
+            }
           }
-        }
-      }
-    }`
+        } 
+    `
 	})
 
 	const products = productQuery.body.data.products.edges.map((edge) => edge.node)
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-  import type { PageData } from "./$types"
+	import type { PageData } from './$types'
 
-  export let data: PageData
+	export let data: PageData
 </script>
 
 {#each data.products as product}
-  <div>
-    <h1>{product.title}</h1>
-    <p>{product.descriptionHtml}</p>
-  </div>
+	<div>
+		<h1>{product.title}</h1>
+		<p>{product.descriptionHtml}</p>
+		<img src={product.images.edges[0].node.url} alt={product.images.edges[0].node.url}/>
+	</div>
 {/each}
-
